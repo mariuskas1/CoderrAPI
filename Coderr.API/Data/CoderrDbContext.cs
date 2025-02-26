d5f2926e94aec1d0c721b2d6b0a18d4680950e62
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-26 15:33:45 +0100

    Added Database

---
+++ Coderr.API/Data/CoderrDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Coderr.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coderr.API.Data
+{
+    public class CoderrDbContext : DbContext
+    {
+        public CoderrDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+
+        }
+
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
