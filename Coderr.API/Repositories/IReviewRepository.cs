aaf7a137a07077281fb8f0d156ef7428f0c72eda
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-02 11:48:20 +0100

    Added ReviewRepository Methods

--- Coderr.API/Repositories/IReviewRepository.cs
+++ Coderr.API/Repositories/IReviewRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Coderr.API.Repositories
+﻿using Coderr.API.Models.Domain;
+
+namespace Coderr.API.Repositories
 {
     public interface IReviewRepository
     {
+        Task<List<Review>> GetAllAsync(Guid businessUserId, string ordering);
+        Task<Review> GetByIdAsync(Guid id);
+        Task<Review> UpdateAsync(Guid id, Review review);
+        Task<Review> DeleteAsync(Guid id);
     }
 }
--- Coderr.API/Repositories/SQLReviewRepository.cs
+++ Coderr.API/Repositories/SQLReviewRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Coderr.API.Data;
+using Coderr.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coderr.API.Repositories
 {
@@ -9,6 +11,63 @@ namespace Coderr.API.Repositories
         public SQLReviewRepository(CoderrDbContext dbContext)
         {
             this.dbContext = dbContext;
+        }
+
+        public async Task<Review> DeleteAsync(Guid id)
+        {
+            var existingReview = await dbContext.Reviews.FirstOrDefaultAsync(x => x.id == id);
+            
+            if (existingReview == null)
+            {
+                return null;
+            }
+
+            dbContext.Reviews.Remove(existingReview);
+            await dbContext.SaveChangesAsync();
+            return existingReview;
+        }
+
+        public async Task<List<Review>> GetAllAsync(Guid businessUserId, string ordering)
+        {
+            var query = dbContext.Reviews
+             .Where(r => r.BusinessUserId == businessUserId);
+
+            if (!string.IsNullOrEmpty(ordering))
+            {
+                if (ordering.ToLower() == "rating")
+                {
+                    query = query.OrderByDescending(r => r.rating);
+                }
+                else if (ordering.ToLower() == "date")
+                {
+                    query = query.OrderByDescending(r => r.created_at);
+                }
+            }
+
+            return await query.ToListAsync();
+
+        }
+
+        public async Task<Review> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Reviews.FirstOrDefaultAsync(r => r.id == id);
+        }
+
+        public async Task<Review> UpdateAsync(Guid id, Review review)
+        {
+            var existingReview = await dbContext.Reviews.FirstOrDefaultAsync(x => x.id == id);
+            
+            if(existingReview == null)
+            {
+                return null;
+            }
+
+            existingReview.rating = review.rating;
+            existingReview.description = review.description;
+
+            await dbContext.SaveChangesAsync();
+            return existingReview;
+           
         }
     }
 }
