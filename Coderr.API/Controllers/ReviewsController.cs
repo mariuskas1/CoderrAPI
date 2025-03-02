f3795172d9992fe1a63a25b897aac1686df85b8e
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-02 12:23:22 +0100

    Added GetAll Method to ReviewsController

--- Coderr.API/Controllers/ReviewsController.cs
+++ Coderr.API/Controllers/ReviewsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Coderr.API.Data;
+using Coderr.API.Models.DTOs;
+using Coderr.API.Models.DTOs.Order;
+using Coderr.API.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Coderr.API.Controllers
@@ -7,5 +12,23 @@ namespace Coderr.API.Controllers
     [ApiController]
     public class ReviewsController : ControllerBase
     {
+        private readonly CoderrDbContext dbContext;
+        private readonly IReviewRepository reviewRepository;
+        private readonly IMapper mapper;
+
+        public ReviewsController(CoderrDbContext dbContext, IReviewRepository reviewRepository, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.reviewRepository = reviewRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] Guid business_user_id, [FromQuery] string? ordering)
+        {
+            var reviewsDomain = await reviewRepository.GetAllAsync(business_user_id, ordering);
+            var reviewsDTO = mapper.Map<List<ReviewDTO>>(reviewsDomain);
+            return Ok(reviewsDTO);
+        }
     }
 }
