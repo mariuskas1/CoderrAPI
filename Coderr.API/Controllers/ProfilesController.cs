2aaebcc1b555b155406a1f644eced6f1a35bf50e
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-02 14:58:14 +0100

    Added Mappings and DTOs to Methods in ProfilesController

--- Coderr.API/Controllers/ProfilesController.cs
+++ Coderr.API/Controllers/ProfilesController.cs
@@ -1,5 +1,7 @@
-﻿using Coderr.API.Data;
+﻿using AutoMapper;
+using Coderr.API.Data;
 using Coderr.API.Models.Domain;
+using Coderr.API.Models.DTOs;
 using Coderr.API.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,33 +14,38 @@ namespace Coderr.API.Controllers
     {
         private readonly CoderrDbContext dbContext;
         private readonly IUserProfileRepository userProfileRepository;
+        private readonly IMapper mapper;
 
-        public ProfilesController(CoderrDbContext dbContext, IUserProfileRepository userProfileRepository)
+        public ProfilesController(CoderrDbContext dbContext, IUserProfileRepository userProfileRepository, IMapper mapper)
         {
             this.dbContext = dbContext;
             this.userProfileRepository = userProfileRepository;
+            this.mapper = mapper;
         }
 
         [HttpGet("/profile/{id:guid}")]
         public async Task<IActionResult> GetProfileById(Guid id)
         {
-            var profile = await userProfileRepository.GetByIdAsync(id);
-            if (profile == null) return NotFound();
-            return Ok(profile);
+            var profileDomain = await userProfileRepository.GetByIdAsync(id);
+            if (profileDomain == null) return NotFound();
+            var profileDTO = mapper.Map<UserProfileDTO>(profileDomain);
+            return Ok(profileDTO);
         }
 
         [HttpGet("business")]
         public async Task<IActionResult> GetBusinessProfiles()
         {
-            var businessProfiles = await userProfileRepository.GetProfilesByTypeAsync(UserProfile.UserType.Business);
-            return Ok(businessProfiles);
+            var businessProfilesDomain = await userProfileRepository.GetProfilesByTypeAsync(UserProfile.UserType.Business);
+            var businessProfilesDTO = mapper.Map<UserProfileDTO>(businessProfilesDomain);
+            return Ok(businessProfilesDTO);
         }
 
         [HttpGet("customer")]
         public async Task<IActionResult> GetCustomerProfiles()
         {
-            var businessProfiles = await userProfileRepository.GetProfilesByTypeAsync(UserProfile.UserType.Customer);
-            return Ok(businessProfiles);
+            var customerProfilesDomain = await userProfileRepository.GetProfilesByTypeAsync(UserProfile.UserType.Customer);
+            var customerProfilesDTO = mapper.Map<UserProfileDTO>(customerProfilesDomain);
+            return Ok(customerProfilesDTO);
         }
 
     }
