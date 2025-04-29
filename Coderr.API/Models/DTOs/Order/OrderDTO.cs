34dfc3966c3a73077042e1b649a41af1623a0f00
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-29 14:56:49 +0200

    Improved OrderDTO

--- Coderr.API/Models/DTOs/Order/OrderDTO.cs
+++ Coderr.API/Models/DTOs/Order/OrderDTO.cs
@@ -7,16 +7,10 @@ namespace Coderr.API.Models.DTOs.Order
     public class OrderDTO
     {
         public Guid Id { get; set; }
-
-        [ForeignKey("customer_user")]
         public string CustomerUserId { get; set; }
         public UserProfile customer_user { get; set; }
-
-
-        [ForeignKey("business_user")]
         public string BusinessUserId { get; set; }
         public UserProfile business_user { get; set; }
-
 
         public Guid offer_detail_id { get; set; }
         public OfferDetails offer_details { get; set; }
@@ -24,15 +18,13 @@ namespace Coderr.API.Models.DTOs.Order
         public string title { get; set; }
         public int revisions { get; set; }
         public int delivery_time_in_days { get; set; }
-
-        [Column(TypeName = "decimal(10,2)")]
         public decimal price { get; set; }
 
         public string features { get; set; } = "[]";
         public string offer_type { get; set; }
         public string status { get; set; } = "in_progress";
-        public DateTime created_at { get; set; } = DateTime.UtcNow;
-        public DateTime updated_at { get; set; } = DateTime.UtcNow;
+        public DateTime created_at { get; set; } 
+        public DateTime updated_at { get; set; } 
         public void SetFeatures(List<string> features)
         {
             this.features = JsonSerializer.Serialize(features);
