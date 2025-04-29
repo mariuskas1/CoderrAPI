25f1d7cc3638e864e511bad86f5146379e27e4fd
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-29 14:42:48 +0200

    Improved OfferDTO

--- Coderr.API/Models/DTOs/Offer/OfferDTO.cs
+++ Coderr.API/Models/DTOs/Offer/OfferDTO.cs
@@ -7,17 +7,13 @@ namespace Coderr.API.Models.DTOs.Offer
     public class OfferDTO
     {
         public Guid id { get; set; }
-
-        [ForeignKey("user")]
         public string UserId { get; set; }
         public UserProfile user { get; set; }
-
-        [Required]
         public string title { get; set; }
         public string? image { get; set; }
         public string description { get; set; }
-        public DateTime created_at { get; set; } = DateTime.Now;
-        public DateTime updated_at { get; set; } = DateTime.UtcNow;
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
 
         public ICollection<OfferDetails> details { get; set; } = new List<OfferDetails>();
     }
