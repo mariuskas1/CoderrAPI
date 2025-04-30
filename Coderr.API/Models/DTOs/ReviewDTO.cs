68a90002991bb65476ff8004a794bff295059bfc
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-30 12:50:52 +0200

    Improved ReviewDTO

--- Coderr.API/Models/DTOs/ReviewDTO.cs
+++ Coderr.API/Models/DTOs/ReviewDTO.cs
@@ -7,15 +7,13 @@ namespace Coderr.API.Models.DTOs
     {
         public Guid id { get; set; }
 
-        [ForeignKey("business_user")]
         public string business_user_id { get; set; }
         public UserProfile business_user { get; set; }
 
-        [ForeignKey("reviewer")]
         public string reviewer_id { get; set; }
         public UserProfile reviewer { get; set; }
         public float rating { get; set; }
         public string description { get; set; }
-        public DateTime created_at { get; set; } = DateTime.UtcNow;
+        public DateTime created_at { get; set; } 
     }
 }
